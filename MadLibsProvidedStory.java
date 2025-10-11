d6101004f415a01f206a7afe76ba501c2db7005f
rishinellipudi <148394052+rishinellipudi@users.noreply.github.com>
2025-10-10 21:39:49 -0700

    Update MadLibsProvidedStory.java
    updated to use indexOf and substring methods rather than replace

--- MadLibsProvidedStory.java
+++ MadLibsProvidedStory.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;
-
 public class MadLibsProvidedStory {
     public static void main(String[] args) {
-        // create scanner object for user input
         Scanner input = new Scanner(System.in);
 
-        // variable containing the incomplete Mad Lib
+        // original story with placeholders
         String story = "I went to the zoo to see a <noun>. There were people <verb> around the enclosure which made the <adjective> apes go bananas.";
 
-        // prompts to keep the user on track
-        System.out.println("Welcome to Mad Libs. please provide the following words:");
-        System.out.print("enter a noun: ");
+        // get user input for each placeholder
+        System.out.print("Enter a noun: ");
         String noun = input.nextLine();
-
-        System.out.print("enter a verb: ");
+        System.out.print("Enter a verb: ");
         String verb = input.nextLine();
-
-        System.out.print("enter an adjective: ");
+        System.out.print("Enter an adjective: ");
         String adjective = input.nextLine();
 
-        // algorithm to replace placeholders with user input using String methods
-        story = story.replace("<noun>", noun);
-        story = story.replace("<verb>", verb);
-        story = story.replace("<adjective>", adjective);
+        // find and replace <noun> using indexOf and substring methods
+        int nounPosition = story.indexOf("<noun>");
+        story = story.substring(0, nounPosition) + noun + story.substring(nounPosition + 6);
 
-        // final print statement to display the completed Mad Lib
-        System.out.println("\nHere is your completed Mad Lib:");
+        // find and replace <verb>
+        int verbPosition = story.indexOf("<verb>");
+        story = story.substring(0, verbPosition) + verb + story.substring(verbPosition + 6);
+
+        // find and replace <adjective>
+        int adjPosition = story.indexOf("<adjective>");
+        story = story.substring(0, adjPosition) + adjective + story.substring(adjPosition + 11);
+
+        // display completed mad lib
+        System.out.println("\nHere is the completed MadLib: ");
         System.out.println(story);
 
-        // close the scanner
         input.close();
     }
 }
